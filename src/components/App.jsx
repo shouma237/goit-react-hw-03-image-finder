58a5ef51c49e52eb6119581476ce0e7cefd19e88
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-02-01 14:43:15 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,16 +14,7 @@ export class App extends Component {
     images: [],
     isLoading: false,
     isError: false,
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const newSearch = e.target.search.value.trim().toLowerCase();
-    this.setState({ search: newSearch, page: 1, images: [] });
-  };
-
-  handleClick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+    isEnd: false,
   };
 
   componentDidUpdate = async (_prevProps, prevState) => {
@@ -36,7 +27,7 @@ export class App extends Component {
 
   fetchImages = async (search, page) => {
     try {
-      this.setState({ isLoading: true, isError: false });
+      this.setState({ isLoading: true });
 
       // fetch data from API
       const fetchedImages = await getAPI(search, page);
@@ -44,27 +35,53 @@ export class App extends Component {
 
       console.log(hits, totalHits);
 
-      if (page < 2) {
+      // Display an error message, if there is no match with the search
+      if (hits.length === 0) {
+        toast.error(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+
+      // Display a success message if it's the first page
+      if (page === 1) {
         toast.success(`Hooray! We found ${totalHits} images!`);
+      }
+
+      // Display a message if page is already at the end of data (12 = per_page based on API call)
+      if (page * 12 >= totalHits) {
+        this.setState({ isEnd: true });
+        toast("We're sorry, but you've reached the end of search results.");
       }
 
       // Update the state with the new images
       this.setState(prevState => ({
         images: [...prevState.images, ...hits],
       }));
-    } catch (err) {
+    } catch {
       this.setState({ isError: true });
     } finally {
       this.setState({ isLoading: false });
     }
   };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const newSearch = e.target.search.value.trim().toLowerCase();
+    this.setState({ search: newSearch, page: 1, images: [] });
+  };
+
+  handleClick = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   render() {
-    const { images, isLoading, isError } = this.state;
+    const { images, isLoading, isError, isEnd } = this.state;
     return (
       <div className={css.app}>
         <Searchbar onSubmit={this.handleSubmit} />
         {images.length > 0 && <ImageGallery photos={images} />}
-        {images.length > 1 && <Button onClick={this.handleClick} />}
+        {images.length > 1 && !isEnd && <Button onClick={this.handleClick} />}
         {isLoading && <Loader />}
         {isError &&
           toast.error('Oops, something went wrong! Reload this page!')}
